bb7254cfe30c8d3477b09b4c368395a227581088
riturajsingh02 <124920823+riturajsingh02@users.noreply.github.com>
2026-09-23 13:06:38 +0530

    q

--- js/core/state.js
+++ js/core/state.js
@@ -6,3 +6,6 @@ let wishlist = JSON.parse(localStorage.getItem('thecandleier_wishlist')) || [];
 let activeCategory = 'all';
 let selectedPaymentMethod = 'prepaid';
 let activePdpProductId = null;
+let activePdpVariantId = null;
+let currentPdpIndex = 0;
+let currentPdpImages = [];
