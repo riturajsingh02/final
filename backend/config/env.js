7997d9b48617303883c8f6614439825dc7e25439
riturajsingh02 <124920823+riturajsingh02@users.noreply.github.com>
2026-09-23 09:36:16 +0530

    d

--- backend/config/env.js
+++ backend/config/env.js
@@ -60,7 +60,8 @@ export const config = {
   oauth: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      callbackUrl: process.env.GOOGLE_CALLBACK_URL || ''
     }
   },
 
